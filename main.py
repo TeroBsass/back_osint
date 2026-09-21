4fb4eacffd4600b52674155cc92a2beb83059e68
TeroBass <kocabasana@gmail.com>
2026-09-21 19:05:43 +0300

    Release v1.4.6

--- main.py
+++ main.py
@@ -776,7 +776,7 @@ def group_send(req: GroupSendRequest):
             row = cur.fetchone()
             if not row:
                 raise HTTPException(403, "Group does not exist!!!")
-            if me["name"] not in _parse_members(row):
+            if me["name"] not in _parse_members(row[0]):
                 raise HTTPException(404, "You are not in this group!!!")
 
             text = f"{me['name']}->{req.text};"
